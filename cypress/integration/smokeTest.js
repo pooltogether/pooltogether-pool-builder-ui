8dce4a04dd3f5a98758a472103256cf4b10b4f2d
Dylan DesRosier <dylan@pooltogether.com>
2021-01-06 17:24:28 -0500

    update

---
+++ cypress/integration/smokeTest.js
@@ -0,0 +1,18 @@
+describe('Smoke Test', () => {
+  it('successfully loads & deploys a USDC pool', () => {
+    cy.visit('/')
+
+    cy.contains('Connect Wallet').click()
+    cy.contains('Show More').click()
+    cy.contains('Web3 Wallet').click()
+    cy.contains('Select the type of prize pool').click()
+    cy.contains('Yield Prize Pool (Compound Protocol)').click()
+    cy.contains('Select a token to be deposited').click()
+    cy.contains('USDC').click()
+    cy.contains('Select a random number generator service').click()
+    cy.contains('Blockhash').click()
+    cy.contains('Create New Prize Pool').click()
+
+    cy.contains('Contracts deployed!', { timeout: 10000 })
+  })
+})
